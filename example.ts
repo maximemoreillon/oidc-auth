76e1adaeac95bf8feac3fbc8284f7fcb0b4e68e4
Maxime Moreillon <m.moreillon@gmail.com>
2024-09-27 09:24:39 +0900

    minor

--- example.ts
+++ example.ts
@@ -1,11 +1,15 @@
 import OidcClient from "./index"
 
-// @ts-ignore
-const { VITE_OIDC_AUTHORITY, VITE_OIDC_CLIENT_ID } = import.meta.env
+const { VITE_OIDC_AUTHORITY, VITE_OIDC_CLIENT_ID, VITE_OIDC_AUDIENCE } =
+  // @ts-ignore
+  import.meta.env
 
 const oidcClient = new OidcClient({
   authority: VITE_OIDC_AUTHORITY,
   client_id: VITE_OIDC_CLIENT_ID,
+  extraQueryParams: {
+    audience: VITE_OIDC_AUDIENCE,
+  },
 })
 
 oidcClient.init().then((user) => {
